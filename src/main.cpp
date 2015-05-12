1e4003c35e1aeb806dc1ae1cbafd4b647bd0d373
m87 <m87blackhole@gmail.com>
2015-05-12 17:58:07 +0200

    excp

--- src/main.cpp
+++ src/main.cpp
@@ -8,54 +8,32 @@ using namespace cv;
 int main(int argc, char *argv[])
 {
 
-    Preprocessor prep(argv[1]);
 
-    Mat past;
-    Mat now;
 
+    Preprocessor prep(argv[1],3);
     namedWindow("capture", CV_WINDOW_AUTOSIZE);
     namedWindow("op", CV_WINDOW_AUTOSIZE);
 
-    BackgroundSubtractorMOG2 fgbg(300,64,false);
-    Mat out, out1; 
-    prep.getFrame(out);
-    prep.getFrame(out1);
-
-    Mat element = getStructuringElement(0,Size(3,3),Point(1,1));
-   Mat element1 = getStructuringElement(MORPH_RECT, Size(7, 7), Point(3,3) );   
-    
-    while(true){
-        prep.getFrame(now);
-        fgbg(now,past,-1);
-       // threshold(past, out, 130,255,0);
-        //dilate(past,out,element);    
-        morphologyEx(past,out,CV_MOP_CLOSE,element1);
+    Mat frameNew, frameLast, frameDiff, freamReal;
+         
 
 
-        vector< vector< Point > >contours;
-        findContours(out,contours,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+        prep.getFrame(frameNew,500.0f, -1.0f, true);
+    while(true) {
+        //prep.skipNFrames(1);
+        frameNew.copyTo(frameLast);    
+        prep.getFrame(frameNew,freamReal,500.0f,-1.0f,  true);
+        frameDiff = frameLast - frameNew;
 
-        vector <Rect> output;
-        vector< vector< Point> >::iterator itc= contours.begin();  
-        while(itc!=contours.end()){
-           
-            Rect mr = boundingRect(Mat(*itc));
-            rectangle(now,mr,CV_RGB(255,0,0));
-            ++itc;
-        }
-
-
-
-
-
-
-        imshow("capture",now);
-        imshow("op",out);
-        if (waitKey(10) >=0 )
-            break;
-
-
-        past=now;
+        
+        prep.saveFrame(frameDiff);
+        
+        imshow("capture", freamReal);
+        imshow("op", frameDiff);
+        if (waitKey(20) >= 0)
+          break;
+        
+        
     }
 
     return 0;
--- src/preprocessor.h
+++ src/preprocessor.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <string>
+#include <vector>
+#include "GenericException.h"
 
 using namespace std;
 using namespace cv;
@@ -11,11 +13,23 @@ class Preprocessor
 {
     /* data */
 
-    VideoCapture *cap;
+    VideoCapture *_cap;
+    vector<Mat> _frameBuffer;
+    unsigned long _frameBufferSize;
+    unsigned long _currentFrameBufferPos;
+
 public:
-    void getFrame(Mat &out);
+    void getFrame(Mat &out, Mat &out_real,float w=-1.0f, float h=-1.0f, bool gray=false);
+    void getFrame(Mat &out,float w=-1.0f, float h=-1.0f, bool gray=false);
+    void skipNFrames(unsigned long n);
+    void saveFrame(Mat frame);
+    void clearFrameBuffer();
+    Mat getFrameFromBuffer(unsigned long n);
+    unsigned long getFrameBufferSize();
+    unsigned long getCurrentFrameBufferSize();
 
     Preprocessor (string file);
+    Preprocessor (string file, unsigned long n);
     virtual ~Preprocessor ();
 
 };
